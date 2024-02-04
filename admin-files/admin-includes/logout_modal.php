4fe30677ed8ecdae8f6d01d2227867e065722c9f
ar4ssii <hcc.paguintojas@gmail.com>
2024-02-04 18:06:43 +0800

    juliana arla paguinto
    -modified buttons to rounded-pill in logout modal

--- admin-files/admin-includes/logout_modal.php
+++ admin-files/admin-includes/logout_modal.php
@@ -2,16 +2,10 @@
 <div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true" data-bs-backdrop="static">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="loginModalLabel">Logout</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                    <h5>Are you sure you want to logout?</h5>
-                </div>
-            <div class="modal-footer">
-                <a href="../index.php" type="submit" class="btn btn-success">Confirm</a>
-                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
+            <div class="modal-body text-center py-5">
+                <h5 class="mb-4">Are you sure you want to logout?</h5>
+                <a href="../index.php" type="submit" class="btn btn-success rounded-pill">Confirm</a>
+                <button type="button" class="btn btn-danger rounded-pill px-4" data-bs-dismiss="modal" aria-label="Close">No</button>
             </div>
         </div>
     </div>
