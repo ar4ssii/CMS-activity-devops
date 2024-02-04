d0f17d824dfe23d0112442dd2044db3f4b5e5dd9
ar4ssii <hcc.paguintojas@gmail.com>
2024-02-04 13:10:55 +0800

    juliana paguinto
    -created interface for cms activity

---
+++ admin-files/admin-includes/sidebar.php
@@ -0,0 +1,13 @@
+ <!-- Side navigation -->
+ <div class="sidenav text-center">
+        <a class="" href="#">
+            <img src="https://webstockreview.net/images/win-clipart-closed-window-13.png" alt="" width="60" height="60">
+        </a>
+        <hr class="hr-custom">
+        <a href="cms.php" data-bs-toggle="tooltip" data-bs-placement="right" title="Content Management"><i class="fas fa-grip-horizontal text-primary"></i></a>
+        <a href="enrollmentSystem.php" data-bs-toggle="tooltip" data-bs-placement="right" title="Digital Enrollment"><i class="fa-solid fa-file text-success"></i></a>
+        <a href="class.php" data-bs-toggle="tooltip" data-bs-placement="right" title="Classroom"><i class="fas fa-chalkboard-teacher text-info"></i></a>
+        <a href="inbox.php" data-bs-toggle="tooltip" data-bs-placement="right" title="Inbox"><i class="fa-solid fa-envelope text-warning"></i></a>
+        
+        <a class="bottom-icon" href="#" data-bs-toggle="tooltip" data-bs-placement="right" title="Logout"><i class="fa-solid fa-right-from-bracket text-danger"></i></a>
+    </div>
---
+++ index-template/login-modal.php
@@ -0,0 +1,24 @@
+<!-- Modal -->
+<div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="loginModalLabel">Login</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <label for="">Username:</label>
+                <div class="input-group mb-2">
+                    <input type="text" class="form-control" name="username" placeholder="Enter your username" id="">
+                </div>
+                <label for="">Password:</label>
+                <div class="input-group mb-2">
+                    <input type="text" class="form-control" name="password" placeholder="Enter your password" id="">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary">Login</button>
+            </div>
+        </div>
+    </div>
+</div>
